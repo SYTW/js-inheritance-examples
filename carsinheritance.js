5ed0866047d0b7a5a45dc60c3161236ad4cc449f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-10-08 10:16:10 +0100

    converted carsinheritance to a module

--- carsinheritance.js
+++ carsinheritance.js
@@ -1,50 +1,55 @@
-function Car(numberOfCylinders, carType){
+(function(exports) {
+  function Car(numberOfCylinders, carType){
 
-  //Private member variables. They can be set only during object construction.
-  var type = carType;
-  var cylinders = numberOfCylinders;
-  var volumeOfCylinder = 250;
+    //Private member variables. They can be set only during object construction.
+    var type = carType;
+    var cylinders = numberOfCylinders;
+    var volumeOfCylinder = 250;
 
-  this.getCC = function(){
-                return cylinders * volumeOfCylinder * this.factor;
-              };
+    this.getCC = function(){
+                  return cylinders * volumeOfCylinder * this.factor;
+                };
 
-  //Public properties. These properties can be set from outside the object.
-  this.color = 'White';
-  this.sunRoof = false;
-  this.factor = 1;
-
-
-}
+    //Public properties. These properties can be set from outside the object.
+    this.color = 'White';
+    this.sunRoof = false;
+    this.factor = 1;
 
 
-  //Shared method
-Car.prototype.start = function(){
-                  console.log('The car with ' + this.getCC() + ' engine and color ' + this.color + ' is starting...');
-              }
-
-Car.prototype.stop = function(){
-                  console.log('The car has stopped');
-                  }
+  }
 
 
+    //Shared method
+  Car.prototype.start = function(){
+                    console.log('The car with ' + this.getCC() + ' engine and color ' + this.color + ' is starting...');
+                }
 
-function Sedan(numberOfCylinder, carType){
-    //Calling the base constructor. All private vars and other properties are initialized. 
-    Car.call(this, numberOfCylinder, carType);
-    //Instance method. The first in the method resolution chain.
-    this.start = function(){
-                     console.log('The Sedan with ' + this.getCC() + ' engine and color ' + this.color + ' is starting...');
-                 }
-}
-
-
-//Set the inheritance - this is the magic line
-Sedan.prototype = new Car();
-Sedan.prototype.constructor = Sedan;
+  Car.prototype.stop = function(){
+                    console.log('The car has stopped');
+                    }
 
 
 
-var honda = new Sedan(4, 'sedan');
-honda.start();
-honda.stop();
+  function Sedan(numberOfCylinder, carType){
+      //Calling the base constructor. All private vars and other properties are initialized. 
+      Car.call(this, numberOfCylinder, carType);
+      //Instance method. The first in the method resolution chain.
+      this.start = function(){
+                       console.log('The Sedan with ' + this.getCC() + ' engine and color ' + this.color + ' is starting...');
+                   }
+  }
+
+
+  //Set the inheritance - this is the magic line
+  Sedan.prototype = new Car();
+  Sedan.prototype.constructor = Sedan;
+
+  exports.Car = Car;
+  exports.Sedan = Sedan;
+
+  /*
+   * var honda = new Sedan(4, 'sedan');
+   * honda.start();
+   * honda.stop();
+  */
+})(this);
