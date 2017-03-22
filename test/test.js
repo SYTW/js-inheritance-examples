514e0a38f227a039781c0016b375a8c32d244549
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-22 11:09:15 +0000

    test

--- test/test.js
+++ test/test.js
@@ -1,79 +1,32 @@
-// Testing TextCell ...
-/*
-var cell = new TextCell("no\nway");
-console.log(cell.heightProp);
-// → 2
-cell.heightProp = 100;
-console.log(cell.heightProp);    
-    
-*/
-var drawTable = require('../lyingouttable.js');
+// Testing drawTable
+var drawTable = require('../lyingoutatable.js');
 
-describe("TextCell", function() {
-  it("should skip prefix spaces", function() {
-    parser.skipSpace("  # hehe\n a").should.equal('a');
+var MOUNTAINS = [
+  {name: "Kilimanjaro\nMontaña mágica", height: 5895, country: "Tanzania"},
+  {name: "Everest", height: 8848, country: "Nepal\nPaís lejano"},
+  {name: "Mount Fuji", height: 3776, country: "Japan"},
+  {name: "Mont Blanc", height: 4808, country: "Italy/France"},
+  {name: "Vaalserberg", height: 323, country: "Netherlands"},
+  {name: "Denali", height: 6168, country: "United States"},
+  {name: "Popocatepetl", height: 5465, country: "Mexico"}
+];
+
+var expected = 
+`name           height country      
+-------------- ------ -------------
+Kilimanjaro    5895   Tanzania     
+Montaña mágica                     
+Everest        8848   Nepal        
+                      País lejano  
+Mount Fuji     3776   Japan        
+Mont Blanc     4808   Italy/France 
+Vaalserberg    323    Netherlands  
+Denali         6168   United States
+Popocatepetl   5465   Mexico       `;
+
+describe("drawTable", function() {
+  it("must draw the mountains table correctly", function() {
+    drawTable(MOUNTAINS).should.equal(expected);
   })
 });
-
-  /*
-  it("should parse apply with multiple arguments", function() {
-    var value = 
-      { expr: 
-        { type: 'apply',
-          operator: { type: 'word', name: 'word' },
-          args:  [ { type: 'word', name: 'a' },
-            { type: 'word', name: 'b' },
-            { type: 'word', name: 'c' } ] },
-        rest: '' }
-    parser.parseExpression('word (a, b, c)').should.eql(value);
-  })
-  it("should parse apply with no args", function() {
-    var value = 
-      { expr: 
-        { type: 'apply',
-          operator: { type: 'word', name: 'word' },
-          args: [] },
-      rest: '' }
-    parser.parseExpression('word ( )').should.eql(value);
-  })
-  it("should apply can be chain", function() {
-    var value = 
-        { expr: 
-          { type: 'apply',
-            operator: { 
-              type: 'apply', 
-              operator: { type: 'word', name: 'word' },
-              args: [ { type: 'word', name: 'a' }, { type: 'word', name: 'b' } ]
-            },
-            args: [ { type: 'word', name: 'c' }, { type: 'word', name: 'd' } ]
-          },
-          rest: '' };
-    parser.parseExpression('word (a , b ) (c, d)').should.eql(value);
-  })
-  */
-  it("should have syntax error if not valid", function() {
-    (function(){parser.parseExpression('')}).should.throw("Not valid Egg expression!");
-    (function(){parser.parseExpression('a(,')}).should.throw("Not valid Egg expression!");
-    (function(){parser.parseExpression('a( )(')}).should.throw("Not valid Egg expression!");
-    (function(){parser.parseExpression('a(, )')}).should.throw("Not valid Egg expression!");
-  })
-  it("should have not expected , or ) if not correct seperator", function() {
-    (function(){parser.parseExpression('word (a b)')}).should.throw("Egg expect ',' or ')'");
-    (function(){parser.parseExpression('a(a')}).should.throw("Egg expect ',' or ')'");
-  })
-  it("parse should throw when there are rest", function(){
-    (function() {parser.parse("+(a,b) e")}).should.throw("Egg unexpected text after program!")
-  })
-  it("parse should work well otherwise", function(){
-    var value = 
-      { type: 'apply',
-        operator: { type: 'word', name: '+' },
-        args: [ { type: 'word', name: 'a' }, { type: 'word', name: 'b' } ] }
-    parser.parse("+(a,b)").should.eql(value);
-  })
-})
-
-// FIXME: 测试闭包特性
-//do(define(f, fun(a, fun(b, +(a, b)))),
-//   print(f(4)(5)))
 
